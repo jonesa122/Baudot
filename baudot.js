b13df4fe23987dadb90715a6f0ee85501e5f0c56
Abby Jones <145145964+jonesa122@users.noreply.github.com>
2024-04-27 01:49:30 -0400

    Functional But Not Cute

--- baudot.js
+++ baudot.js
@@ -7,36 +7,36 @@ function crypt(message, key){
         let tempResult = messageNumber ^ keyNumber;
         result.push(tempResult);
     }
+    console.log(result);
     return result;
 }
     
 function decode(encoded){
     var decoded = [];
-    const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
-        ,"r","s","t","u","v","w","x","y","z","shiftF"];
+    const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","shiftF"];
     const binary = [24, 19, 28, 18, 16, 22, 11, 5, 12, 26, 30, 9, 7, 6, 3, 13, 29, 10, 20, 1, 28, 15, 25, 23, 21, 17, 27]; 
-    const figures = ["-","?",":","who are you?","3","%","@","$","8","ring bell"
-        ,"(",")",".",",","9","0","1","4","'","5","7","=","2","/","6","+","shiftL"];
+    const figures = ["-","?",":","who are you?","3","%","@","$","8","ring bell","(",")",".",",","9","0","1","4","'","5","7","=","2","/","6","+","shiftL"];
     const finary = [24, 19, 28, 18, 16, 22, 11, 5, 12, 26, 30, 9, 7, 6, 3, 13, 29, 10, 20, 1, 28, 15, 25, 23, 21, 17,  31];
-    const specialCharacter = [" ", "\n", "line feed"];
-    const special = [4, 2, 8];
+    const specialCharacter = ["/", " ", "\n", "line feed"];
+    const special = [0, 4, 2, 8];
 
     var isLetter = true;
     for(let i = 0; i < encoded.length; i++){
         var found = false;
-        var encodedNumber = encoded.get(i);
+        var encodedNumber = encoded[i];
         for (let j = 0; j<special.length && !found; j++){
             if(encodedNumber == special[j]){
                 found = true;
-                decoded = decoded.push(specialCharacter[j]);
+                decoded.push(specialCharacter[j]);
             }
         }
         if(!found){
             if(isLetter){
                 for(let j=0; j < binary.length && !found; j++){
                     if(encodedNumber == binary[j]){
+                        console.log(letters[j]);
                         found = true;
-                        decoded = decoded.push(letters[j]);
+                        decoded.push(letters[j]);
                         if(j == 26){
                             isLetter = false;
                         }
@@ -46,7 +46,7 @@ function decode(encoded){
                 for(let j=0; j < finary.length && !found; j++){
                     if(encodedNumber ==finary[j]){
                         found = true;
-                        decoded = decoded.push(figures[j]);
+                        decoded.push(figures[j]);
                         if(j == 26){
                             isLetter = true;
                         }
@@ -66,8 +66,8 @@ function encode(message){
     const figures = ["-","?",":","who are you?","3","%","@","$","8","ring bell"
         ,"(",")",".",",","9","0","1","4","'","5","7","=","2","/","6","+","shiftL"];
     const finary = [24, 19, 28, 18, 16, 22, 11, 5, 12, 26, 30, 9, 7, 6, 3, 13, 29, 10, 20, 1, 28, 15, 25, 23, 21, 17,  31];
-    const specialCharacter = [" ", "\n", "line feed"];
-    const special = [4, 2, 8];
+    const specialCharacter = ["/", " ", "\n", "line feed"];
+    const special = [0, 4, 2, 8];
 
     var firstCharacter="";
     var firstSix="";
@@ -116,6 +116,8 @@ function encode(message){
     } else{
         encodeSpecial(encoded, message, 0);
     }
+
+    console.log(encoded);
 
     return encoded;
 }
@@ -199,8 +201,8 @@ function encodeFigures(encoded, message, index){
 }
 
 function encodeSpecial(encoded, message, index){
-    const specialCharacter = [" ", "\n", "line feed"];
-    const special = [4, 2, 8];
+    const specialCharacter = ["/", " ", "\n", "line feed"];
+    const special = [0, 4, 2, 8];
     const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","shiftF"];
     var isLetters = false;
     var isFigures = false;
@@ -244,4 +246,182 @@ function printBinary(encoded){
         final = final.concat(binary);
     }
     return final;
+}
+
+function printMessage(decoded){
+    var final = "";
+    for(let i = 0; i < decoded.length; i++){
+        final = final.concat(decoded[i]);
+    }
+    return final;
+}
+
+function determineInputOutput(inputIndicator, outputIndicator, input, key){
+    var i;
+    var j;
+    var inputSelected;
+    var outputSelected;
+    for(i = 0; i < inputIndicator.length; i++){
+        if(inputIndicator[i].checked){
+            inputSelected = inputIndicator[i].value;
+        }
+    }
+    for(j = 0; j < outputIndicator.length; j++){
+        if(outputIndicator[j].checked){
+            outputSelected = outputIndicator[j].value;
+        }
+    }
+    console.log(inputSelected);
+    console.log(outputSelected);
+    console.log(input);
+    console.log(key);
+
+    if(inputSelected === "englishPlaintextInput"){
+        if(outputSelected === "englishPlaintextOutput"){
+            return input;
+        } else if(outputSelected === "binaryPlaintextOutput"){
+            return printBinary(encode(input));
+        } else if(outputSelected === "englishCiphertextOutput"){
+            return printMessage(decode(crypt(encode(input), encode(key))));
+        } else{
+            return printBinary(crypt(encode(input), encode(key)));
+        }
+    } else if(inputSelected === "binaryPlaintextInput"){
+        let inputArray = [];
+        let keyArray = [];
+        for(let i = 0; i < input.length; i += 5){
+            let firstDigit = input[i];
+            let secondDigit = input[i + 1];
+            let thirdDigit = input[i + 2];
+            let fourthDigit = input[i + 3];
+            let fifthDigit = input[i + 4];
+            let number = 0;
+            if(firstDigit === "1"){
+                number = number + 16;
+            }
+            if(secondDigit === "1"){
+                number = number + 8;
+            }
+            if(thirdDigit === "1"){
+                number = number + 4;
+            }
+            if(fourthDigit === "1"){
+                number = number + 2;
+            }
+            if(fifthDigit === "1"){
+                number = number + 1;
+            }
+            inputArray.push(number);
+        }
+
+        for(let i = 0; i < key.length; i += 5){
+            let firstDigit = key[i];
+            let secondDigit = key[i + 1];
+            let thirdDigit = key[i + 2];
+            let fourthDigit = key[i + 3];
+            let fifthDigit = key[i + 4];
+            let number = 0;
+            if(firstDigit === "1"){
+                number = number + 16;
+            }
+            if(secondDigit === "1"){
+                number = number + 8;
+            }
+            if(thirdDigit === "1"){
+                number = number + 4;
+            }
+            if(fourthDigit === "1"){
+                number = number + 2;
+            }
+            if(fifthDigit === "1"){
+                number = number + 1;
+            }
+            keyArray.push(number);
+        }
+
+        if(outputSelected === "binaryPlaintextOutput"){
+            return printBinary(inputArray);
+        } else if(outputSelected === "englishPlaintextOutput"){
+            return printMessage(decode(inputArray));
+        } else if(outputSelected === "binaryCiphertextOutput"){
+            return printBinary(crypt(inputArray, keyArray));
+        } else{
+            return printMessage(decode(crypt(inputArray, keyArray)));
+        }
+
+    } else if(inputSelected === "englishCiphertextInput"){
+        if(outputSelected === "englishCiphertextOuput"){
+            return input;
+        } else if(outputSelected === "binaryCiphertextOutput"){
+            return printBinary(encode(input));
+        } else if(outputSelected === "englishPlaintextOuput"){
+            return printMessage(decode(crypt(encode(input), encode(key))));
+        } else{
+            return printBinary(crypt(encode(input), encode(key)));
+        }
+
+    } else{
+        let inputArray = [];
+        let keyArray = [];
+        for(let i = 0; i < input.length; i += 5){
+            let firstDigit = input[i];
+            let secondDigit = input[i + 1];
+            let thirdDigit = input[i + 2];
+            let fourthDigit = input[i + 3];
+            let fifthDigit = input[i + 4];
+            let number = 0;
+            if(firstDigit === "1"){
+                number = number + 16;
+            }
+            if(secondDigit === "1"){
+                number = number + 8;
+            }
+            if(thirdDigit === "1"){
+                number = number + 4;
+            }
+            if(fourthDigit === "1"){
+                number = number + 2;
+            }
+            if(fifthDigit === "1"){
+                number = number + 1;
+            }
+            inputArray.push(number);
+        }
+
+        for(let i = 0; i < key.length; i += 5){
+            let firstDigit = key[i];
+            let secondDigit = key[i + 1];
+            let thirdDigit = key[i + 2];
+            let fourthDigit = key[i + 3];
+            let fifthDigit = key[i + 4];
+            let number = 0;
+            if(firstDigit === "1"){
+                number = number + 16;
+            }
+            if(secondDigit === "1"){
+                number = number + 8;
+            }
+            if(thirdDigit === "1"){
+                number = number + 4;
+            }
+            if(fourthDigit === "1"){
+                number = number + 2;
+            }
+            if(fifthDigit === "1"){
+                number = number + 1;
+            }
+            keyArray.push(number);
+        }
+
+        if(outputSelected === "binaryCiphertextOutput"){
+            return printBinary(inputArray);
+        } else if(outputSelected === "englishCiphertextOutput"){
+            return printMessage(decode(inputArray));
+        } else if(outputSelected === "binaryPlaintextOutput"){
+            return printBinary(crypt(inputArray, keyArray));
+        } else{
+            return printMessage(decode(crypt(inputArray, keyArray)));
+        }
+    }
+
 }
